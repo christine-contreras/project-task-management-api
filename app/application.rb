2f39a78e154f9cc3797e7db80629fbfbb010fd9e
Christine Contreras <christineec23@yahoo.com>
2021-09-15 10:44:55 -0500

    initial files

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/test/) 
+      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+
+    else
+      resp.write "Path Not Found"
+
+    end
+
+    resp.finish
+  end
+
+end
---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+require 'bundler/setup'
+
+Bundler.require
+
+require_all 'app'
